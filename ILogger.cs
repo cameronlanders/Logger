518540f805875e51fa6d0272c033f4f975ea8605
Cameron Landers <cameron@sdmsoftware.com>
2023-02-18 13:21:46 -0600

    Minor updates

---
+++ ILogger.cs
@@ -0,0 +1,16 @@
+﻿namespace LoggerTest
+{
+    public interface ILogger
+    {
+        // -----------------------------------------------------
+        // ILogger defines the elements of a basic log class.
+        // Classes that implement the ILogger interface must
+        // match this signature.
+        // -----------------------------------------------------
+        string Logged { get; set; }
+        string Message { get; set; }
+        string Path { get; set; }
+
+        void AppendLog();
+    }
+}
